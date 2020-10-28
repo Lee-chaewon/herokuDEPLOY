384533170a19b0baac36cef1ae7f7e4c085edb8b
Chaewon Lee <chaewon5735@likelion.org>
2020-10-28 21:04:16 +0900

    .

--- blog_ckeditor/settings.py
+++ blog_ckeditor/settings.py
@@ -129,5 +129,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import dj_database_url
 
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
